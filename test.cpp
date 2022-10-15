da9ff043811f72cb9b71499a5d3b36432ea76d63
Shreekumar7272 <115860296+Shreekumar7272@users.noreply.github.com>
2022-10-15 11:54:32 +0530

    Add files via upload
    test.cpp

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+  // int a = 4;
+  // int b = 5;
+  int a = 15, b = 16;
+  float pi=3.14;
+  char c = 's';
+  cout<<"this is cpp 2. \n the value of a is"<<a<<"\nthe value of b is"<<b;
+  cout<<"\nthe value of pi is:"<<pi;
+  cout<<"\nthe value of pi is:"<<c;
+}
+
+
+  
